1b19fe0a5f9d046aa3bac4f7ff7d8cbd777c22f8
Hruthik S <team.hruthiksivakumar@gmail.com>
2020-07-26 09:53:28 +0530

    Add Data Commpleted

--- app/src/main/java/com/example/sqlliteapp/DatabaseHelper.java
+++ app/src/main/java/com/example/sqlliteapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.sqlliteapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -16,18 +17,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME,null, 1);
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table" + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXIST" + TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(String name,String surname,String marks)
+    {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,name);
+        contentValues.put(COL3,surname);
+        contentValues.put(COL4,marks);
+        long result = sqLiteDatabase.insert(TABLE_NAME,null,contentValues);
+        if(result ==-1)
+        {
+            return false;
+        }
+        else
+            return true;
     }
 }
--- app/src/main/java/com/example/sqlliteapp/MainActivity.java
+++ app/src/main/java/com/example/sqlliteapp/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.sqlliteapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDB;
+    EditText namebox,surnamebox,marksbox;
+    Button adddata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDB = new DatabaseHelper(this);
+        namebox=(EditText)findViewById(R.id.namebox);
+        surnamebox=(EditText)findViewById(R.id.surnamebox);
+        marksbox=(EditText)findViewById(R.id.marksbox);
+        adddata=(Button)findViewById(R.id.adddata);
+        AddData();
+    }
+    public void AddData(){
+        adddata.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Boolean isInserted = myDB.insertData(namebox.getText().toString(),surnamebox.getText().toString(),marksbox.getText().toString());
+                        if(isInserted=true)
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data Insert Failed",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
